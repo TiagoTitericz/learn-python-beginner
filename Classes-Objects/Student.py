36092a8720829770a77791dee54fc8f009410b33
TiagoTitericz <tiagotitericz@gmail.com>
2021-02-03 23:06:25 -0300

    Added Classes File
    Classes & Object Tutorial

---
+++ Classes-Objects/Student.py
@@ -0,0 +1,7 @@
+#This is the Student.py program where I created de Class Student
+class Student:
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
---
+++ Classes-Objects/app_clasess.py
@@ -0,0 +1,6 @@
+#In this program I imported the Class Student and set an objext student1
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+
+print(student1.name) #Jim will be printed out
